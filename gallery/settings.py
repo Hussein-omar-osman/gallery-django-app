0bd35607ef119f7ae097620b2c399bca5ac7453e
Hussein Omar <husseinomar6190@gmail.com>
2022-05-27 20:55:09 +0300

    creating a local postgres database and migrate

--- gallery/settings.py
+++ gallery/settings.py
@@ -76,11 +76,22 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'gallery_web',
+      'USER':'postgres',
+      'PASSWORD':'mwas6190',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
 
 
