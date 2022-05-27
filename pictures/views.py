036a065d7e84be78fa3084dbc61f92dff827d951
Hussein Omar <husseinomar6190@gmail.com>
2022-05-27 12:45:33 +0300

    connecting pictures app templates to the gallery project

--- pictures/views.py
+++ pictures/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
+def homePage(request):
+   
+   context = {}
+   return render(request, 'pictures/home.html', context)
