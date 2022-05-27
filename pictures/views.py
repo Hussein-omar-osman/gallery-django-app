42b4be13bdbce75360f59d6486059248508fce39
Hussein Omar <husseinomar6190@gmail.com>
2022-05-27 16:44:44 +0300

    adding static folder for css and js

--- pictures/views.py
+++ pictures/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect, render
 
 # Create your views here.
 def homePage(request):
-   
    context = {}
    return render(request, 'pictures/home.html', context)
+  
+  
