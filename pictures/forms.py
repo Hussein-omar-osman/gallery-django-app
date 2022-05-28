31c657ac2d895b523d2755899ddf965797340523
Hussein Omar <husseinomar6190@gmail.com>
2022-05-28 23:53:27 +0300

    styling the form model using widgets

--- pictures/forms.py
+++ pictures/forms.py
@@ -1,7 +1,17 @@
+from unicodedata import name
 from django.forms import ModelForm
 from .models import Image 
 
 class ImageForm(ModelForm):
+  
+  def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs.update({'class': 'form-control form-control-sm'})
+        self.fields['location'].widget.attrs.update({'class': 'form-control form-control-sm'})
+        self.fields['category'].widget.attrs.update({'class': 'form-control form-control-sm'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control form-control-sm'})
+        self.fields['picture'].widget.attrs.update({'class':"form-control" ,'type':"file", 'id':"formFile"})
   class Meta:
    model = Image
    fields = '__all__'
+   
